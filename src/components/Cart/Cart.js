4c1e201ace9244caf9af8e637461556ae3ed5278
ducan9x <ducan141599@gmail.com>
2021-01-26 14:59:00 +0700

    update cartpage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ class Cart extends Component {
             <div className="cart-container">
                 <div className="cart-container-body">
                     <div className="cart-container-body-title">
-                    <img src='./image/vnb_logo_2x.png' />
+                        <img src='./image/vnb_logo_2x.png' />
                     </div>
                     <h2>Giỏ Hàng</h2>
                     <div className="cart-container-body-content">
@@ -15,20 +15,69 @@ class Cart extends Component {
                             <div className="cart-book">
                                 <img className="img-cart" src="./image/duongmaytrendathoa-01-bia-1.png" />
                                 <p>Lãnh Đạo Và Văn Hóa Doanh Nghiệp<span>1x144000đ</span></p>
-                                <div className="buttons_added">
-                                    {/* <span>Số lượng </span> */}
+                                <p className="soluong">Số lượng: 1</p>
+                                {/* <div className="buttons_added">
                                     <input class="minus is-form" type="button" value="-" />
                                     <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1" />
                                     <input class="plus is-form" type="button" value="+" />
-                                </div>                            
+                                </div> */}
+                                <button>Xóa</button><button>Sửa</button>
                             </div>
+
+                            <div className="cart-book">
+                                <img className="img-cart" src="./image/duongmaytrendathoa-01-bia-1.png" />
+                                <p>Lãnh Đạo Và Văn Hóa Doanh Nghiệp<span>1x144000đ</span></p>
+                                <p className="soluong">Số lượng: 1</p>
+                                {/* <div className="buttons_added">
+                                    <input class="minus is-form" type="button" value="-" />
+                                    <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1" />
+                                    <input class="plus is-form" type="button" value="+" />
+                                </div> */}
+                                <button>Xóa</button><button>Sửa</button>
+                            </div>
+                            <div className="cart-book">
+                                <img className="img-cart" src="./image/duongmaytrendathoa-01-bia-1.png" />
+                                <p>Lãnh Đạo Và Văn Hóa Doanh Nghiệp<span>1x144000đ</span></p>
+                                <p className="soluong">Số lượng: 1</p>
+                                {/* <div className="buttons_added">
+                                    <input class="minus is-form" type="button" value="-" />
+                                    <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1" />
+                                    <input class="plus is-form" type="button" value="+" />
+                                </div> */}
+                                <button>Xóa</button><button>Sửa</button>
+                            </div>
+
+
                         </div>
                         <div className="tt-cart">
-
+                            <div className="tt-cart-title">
+                                <span>Tóm tắt đơn hàng</span>
+                            </div>
+                            <table className="table">
+                                <tr>
+                                    <td className="left">Sản phẩm</td>
+                                    <td className="right">1</td>
+                                  
+                                </tr>
+                                <tr>
+                                    <td className="left">Phí vận chuyển</td>
+                                    <td className="right">Miễn phí</td>
+                                  
+                                </tr>
+                                <tr>
+                                    <td className="left">Tạm tính</td>
+                                    <td className="right">144000 đ</td>
+                                </tr>
+                                
+                            </table>
+                            <div className="total-price">
+                                    <p>Tổng cộng
+                                    <span>144000 đ</span></p>
+                                </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         );
     }
